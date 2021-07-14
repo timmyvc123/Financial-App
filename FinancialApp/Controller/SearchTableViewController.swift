adc7e00e5ee661d359a4074ecbdc978e1eefd11e
Tim Van Cauwenberge <cloutking619@gmail.com>
2021-07-13 22:57:49 -0700

    Added search

--- FinancialApp/Controller/SearchTableViewController.swift
+++ FinancialApp/Controller/SearchTableViewController.swift
@@ -10,6 +10,11 @@ import Combine
 
 class SearchTableViewController: UITableViewController {
     
+    private enum Mode {
+        case onboarding
+        case search
+    }
+    
     private lazy var searchController: UISearchController = {
         let sc = UISearchController(searchResultsController: nil)
         sc.searchResultsUpdater = self
@@ -23,37 +28,69 @@ class SearchTableViewController: UITableViewController {
     
     private let apiService = APIService()
     private var subscribers = Set<AnyCancellable>()
+    private var searchResults: SearchResults?
+    @Published private var mode: Mode = .onboarding
+    @Published private var searchQuery = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
-        performSearch()
+        setupTableView()
+        observeForm()
     }
 
     private func setupNavigationBar() {
         navigationItem.searchController = searchController
     }
     
-    private func performSearch() {
-        
-        apiService.fetchSymbolsPublisher(keywords: "S&P500").sink { (completion) in
-            switch completion {
-            case .failure(let error):
-                print(error.localizedDescription)
-            case .finished: break
-            }
-        } receiveValue: { (searchResults) in
-            print(searchResults)
-        }.store(in: &subscribers)
+    private func setupTableView() {
+        tableView.tableFooterView = UIView()
     }
     
+    private func observeForm() {
+        
+        $searchQuery
+            .debounce(for: .milliseconds(750), scheduler: RunLoop.main)
+            .sink { [unowned self] (searchQuery) in
+                
+                self.apiService.fetchSymbolsPublisher(keywords: searchQuery).sink { (completion) in
+                    switch completion {
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    case .finished: break
+                    }
+                } receiveValue: { (searchResults) in
+                    self.searchResults = searchResults
+                    self.tableView.reloadData()
+                }.store(in: &subscribers)
+                
+            }.store(in: &subscribers)
+        
+        $mode.sink { [unowned self] (mode) in
+            switch mode {
+            case .onboarding:
+                let redView = UIView()
+                redView.backgroundColor = .red
+                self.tableView.backgroundView = redView
+            case .search:
+                self.tableView.backgroundView = nil
+            }
+        }.store(in: &subscribers)
+        
+    }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! SearchTableViewCell
+        if let searchResults = self.searchResults {
+            let searchResult = searchResults.items[indexPath.row]
+            cell.configure(with: searchResult)
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        //if there are now search results return 0
+        return searchResults?.items.count ?? 0
     }
 
 }
@@ -61,6 +98,13 @@ class SearchTableViewController: UITableViewController {
 extension SearchTableViewController: UISearchResultsUpdating, UISearchControllerDelegate {
     
     func updateSearchResults(for searchController: UISearchController) {
+        guard let searchQuery = searchController.searchBar.text,
+              !searchQuery.isEmpty else { return }
         
+        self.searchQuery = searchQuery
+    }
+    
+    func willPresentSearchController(_ searchController: UISearchController) {
+        mode = .search
     }
 }
--- FinancialApp/View/SearchTableViewCell.swift
+++ FinancialApp/View/SearchTableViewCell.swift
@@ -13,4 +13,12 @@ class SearchTableViewCell: UITableViewCell {
     @IBOutlet weak var assetSymbol: UILabel!
     @IBOutlet weak var assetTypeLabel: UILabel!
     
+    func configure(with searchResult: SearchResult) {
+        assetNameLabel.text = searchResult.name
+        assetSymbol.text = searchResult.symbol
+        assetTypeLabel.text = searchResult.type
+            .appending(" ")
+            .appending(searchResult.currency)
+    }
+    
 }
